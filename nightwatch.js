7c3eea539a4201eb425239ea848a960cb5ba1248
Jaifroid <egk10@cam.ac.uk>
2021-02-20 14:19:39 +0000

    Add timeout_options to nightwatch default settings

--- nightwatch.js
+++ nightwatch.js
@@ -44,6 +44,12 @@ module.exports = {
       },
       "globals": {
         "waitForConditionTimeout": 600
+      },
+      // Configure when a request to the Selenium server should time out and optionally define the number of retries for a timed-out request
+      // See https://github.com/nightwatchjs/nightwatch/issues/1936
+      request_timeout_options: {
+        timeout: 100000,
+        retry_attempts: 3
       }
     },
     "firefox45" : {
